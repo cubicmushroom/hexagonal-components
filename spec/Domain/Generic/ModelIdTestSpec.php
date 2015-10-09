16d91ad53c75ce92e553290e7686965ecd43021f
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-09 10:55:40 +0100

    Moved test class into Spec file

--- spec/Domain/Generic/ModelIdTestSpec.php
+++ spec/Domain/Generic/ModelIdTestSpec.php
@@ -1,23 +1,38 @@
 <?php
 
-namespace spec\CubicMushroom\Hexagonal\Domain\Generic;
+namespace spec\CubicMushroom\Hexagonal\Domain\Generic {
 
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+    use PhpSpec\ObjectBehavior;
+    use Prophecy\Argument;
 
-/**
- * Class ModelIdTestSpec
- *
- * @package CubicMushroom\Hexagonal
- *
- * @see     \CubicMushroom\Hexagonal\Domain\Generic\ModelIdTest
- */
-class ModelIdTestSpec extends ObjectBehavior
-{
-
-    function it_should_be_possible_to_construct_with_an_existing_value()
+    /**
+     * Class ModelIdTestSpec
+     *
+     * @package CubicMushroom\Hexagonal
+     *
+     * @see     \CubicMushroom\Hexagonal\Domain\Generic\ModelIdTest
+     */
+    class ModelIdTestSpec extends ObjectBehavior
     {
-        /** @noinspection PhpMethodParametersCountMismatchInspection */
-        $this->beConstructedWith('123');
+
+        function it_should_be_possible_to_construct_with_an_existing_value()
+        {
+            /** @noinspection PhpMethodParametersCountMismatchInspection */
+            $this->beConstructedWith('123');
+        }
+    }
+}
+
+namespace CubicMushroom\Hexagonal\Domain\Generic {
+
+    /**
+     * Class ModelIdTest
+     *
+     * @package CubicMushroom\Hexagonal
+     *
+     * @see     \spec\CubicMushroom\Hexagonal\Domain\Generic\ModelIdTestSpec
+     */
+    class ModelIdTest extends ModelId
+    {
     }
 }
