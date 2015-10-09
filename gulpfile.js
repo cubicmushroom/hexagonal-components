6fa4a1524aca46cdac4ff1507815745f5d80ed1b
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-09 09:27:16 +0100

    Started on Gulp task to create PHPSpec descriptions with namespace prefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,44 @@
 var gulp    = require('gulp'),
     notify  = require('gulp-notify'),
-    phpspec = require('gulp-phpspec');
+    phpspec = require('gulp-phpspec'),
+    yargs   = require('yargs');
+
+var namespace = 'CubicMushroom\\Hexagonal\\';
+
+//console.dir(yargs);
+
+yargs.options({
+  'c': {
+    alias   : 'class',
+    describe: 'Class to describe',
+    type    : 'string', /* array | boolean | string */
+    nargs   : 1
+  }
+});
+if (!yargs.argv.h) {
+  yargs.demand('c', 'Class (-c, --class) is required');
+}
+
+var argv = yargs.argv;
+
+
+// -----------------------------------------------------------------------------------------------------------------
+// PHPSpec Tasks
+// -----------------------------------------------------------------------------------------------------------------
 
 var phpspecGlob = 'spec/**/*Spec.php';
 var srcGlob = 'src/**/*.php';
 
+gulp.task('desc', function () {
+  if (argv.h || !argv.c) {
+    yargs.showHelp();
+    return;
+  }
+
+
+});
+
+// phpspec
 gulp.task('phpspec', function () {
   var options = {debug: true, notify: true};
 
@@ -16,6 +50,7 @@ gulp.task('phpspec', function () {
     }));
 });
 
+// watch
 gulp.task('watch', function () {
   gulp.watch([phpspecGlob, srcGlob], ['phpspec']);
 });
